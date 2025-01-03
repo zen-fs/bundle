01cffbbdffdff23093a7a678d4ae59b92aef5d7b
James Prevett <jp@jamespre.dev>
2025-01-03 09:36:02 -0600

    Updated version.sh to prompt for asking the user to publish

--- version.sh
+++ version.sh
@@ -4,3 +4,12 @@
 npm version 1.0.0-date-$(date +%Y.%m.%d)
 
 npm publish --access public --dry-run --tag latest
+
+read -p "Do you want to continue and publish? (y/N) " -n 1 -r
+
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	npm publish --access public --tag latest
+else
+	echo "Publish aborted."
+fi
