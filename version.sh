5183d65b8c5813ffd98c22d0ca2055f9c904934c
James Prevett <jp@jamespre.dev>
2025-01-06 20:10:59 -0600

    Add explicit tag to version.sh

--- version.sh
+++ version.sh
@@ -5,13 +5,13 @@ npm install @zenfs/archives@latest @zenfs/cloud@latest @zenfs/core@latest @zenfs
 # x.x.x-date-yyyy.mm.dd
 npm version 1.0.0-date-$(date +%Y.%m.%d)
 
-npm publish --dry-run
+npm publish --dry-run --tag latest
 
 read -p "Do you want to continue and publish? (y/N) " -n 1 -r
 
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-	npm publish
+	npm publish --tag latest
 else
 	echo "Publish aborted."
 fi
